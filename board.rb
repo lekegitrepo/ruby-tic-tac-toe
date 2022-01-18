cbda07278d34dd3b5e28e2b096529be0ad2b25b7
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-01-18 08:46:18 +0100

    Create row_win method

--- board.rb
+++ board.rb
@@ -18,9 +18,42 @@ class Board
     end
     puts
   end
+
+  def row_win?(marker)
+    @grid.any? do |row|
+      row.all? do |cell|
+        cell == marker
+      end
+    end
+  end
+
+  def column_win?(marker)
+    
+  end
+
+  def diagonal_win?(marker)
+    
+  end
 end
 
-board = Board.new
-board.print_grid
-board.grid[0][1] = :X
-board.print_grid
+def check_and_print(current_maker)
+  @board.print_grid
+  @board.row_win?(current_maker)
+end
+
+@board = Board.new
+p check_and_print(:X)
+@board.grid[0][0] = :X
+p check_and_print(:X)
+@board.grid[0][1] = :X
+p check_and_print(:X)
+@board.grid[0][2] = :X
+p check_and_print(:X)
+puts "X end"
+puts "O start"
+@board.grid[1][0] = :O
+p check_and_print(:O)
+@board.grid[1][1] = :O
+p check_and_print(:O)
+@board.grid[1][2] = :O
+p check_and_print(:O)

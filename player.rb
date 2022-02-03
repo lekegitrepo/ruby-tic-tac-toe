a78446d1ad995ff692ec0f512c2345350881eab9
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-02-03 09:05:11 +0100

    Refactor get_row and get_column

--- player.rb
+++ player.rb
@@ -4,6 +4,11 @@ class Player
 
   ROW_MAP = (:A..:C).zip(0..2).to_h
 
+  TRANSLATIONS = {
+    row: -> (input) { ROW_MAP[input[0].upcase.to_sym] },
+    column: -> (input) { input.to_i - 1 }
+  }
+
   attr_reader :marker
 
   def initialize(name, marker)
@@ -13,41 +18,47 @@ class Player
   end
 
   def get_move
-    [get_row, get_col]
+    [get_coordinates(:row), get_coordinates(:column)]
+    TRANSLATIONS.map do |row_or_column, translator|
+      get_coordinates(row_or_column, translator)
+    end
   end
 
-  def get_row
-    player_move_prompt('row')
+  private
+
+  def get_coordinates(row_or_column, translator)
+    print player_move_prompt(row_or_column)
     loop do
       input = gets
-      choice = translation_row(input)
+      choice = translator.call(input)
+      # method_to_call = "translate_#{row_or_column}".to_sym
+      # choice = send(method_to_call, input)
       return choice if in_bounds?(choice)
       puts 'invalid move'
     end
   end
 
-  def get_col
-    player_move_prompt('column')
-    input = gets
-    choice = translation_column(input)
-    return choice if in_bounds?(choice)
-    puts 'invalid move'
-  end
+  # def get_col
+  #   player_move_prompt('column')
+  #   input = gets
+  #   choice = translation_column(input)
+  #   return choice if in_bounds?(choice)
+  #   puts 'invalid move'
+  # end
 
-  def translation_row(input)
-    ROW_MAP[input[0].upcase.to_sym]
-  end
+  # def translate_row(input)
+  #   ROW_MAP[input[0].upcase.to_sym]
+  # end
 
-  def translation_column(input)
-    input.to_i - 1
-  end
+  # def translate_column(input)
+  #   input.to_i - 1
+  # end
 
   def in_bounds?(choice)
     (0..2).include?(choice)
   end
 
-  def player_move_prompt(string)
-    puts "Pick a #{string}"
-    print '> '
+  def player_move_prompt(row_or_column)
+    "Pick a #{row_or_column} \n >"
   end
 end

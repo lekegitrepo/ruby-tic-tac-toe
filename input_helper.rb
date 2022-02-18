2df511fab3ada585afffa8937711ab27f4386fcf
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-02-18 09:01:25 +0100

    Implement game exit functionality

--- input_helper.rb
+++ input_helper.rb
@@ -1,5 +1,18 @@
 module InputHelper
+
+  SAFE_WORD = ['QUIT', 'EXIT']
+
   def prompt(string)
     print(string + "\n> ")
+  end
+
+  def gets
+    input = Kernel.gets
+    if SAFE_WORD.include?(input.chomp.strip.upcase)
+      puts 'You quit the game!'
+      puts 'Goodbye !!!'
+      exit
+    end
+    input
   end
 end
